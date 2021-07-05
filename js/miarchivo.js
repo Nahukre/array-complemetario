7b49aa4942cac9b52721aed00a1f7443f7bdd354
Nahukre <79340552+Nahukre@users.noreply.github.com>
2021-07-05 20:47:30 -0300

    completo

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -9,3 +9,23 @@ const ordenAlfabetico = total.sort();
 console.log(ordenAlfabetico);
 const cadena = ordenAlfabetico.join(" ");
 console.log(cadena.toUpperCase());
+
+
+
+const opcionesDeInversion = [
+    { nombre: "Dolar blue", valor: 170 },
+    { nombre: "Bitcoin", valor: 7000000 },
+    { nombre: "Testa cedear", valor: 7500 },
+    { nombre: "Apple cedear", valor: 3500 },
+    { nombre: "Disney cedear", valor: 4500 },
+];
+opcionesDeInversion.sort((a, b) => {
+    if (a.valor < b.valor) {
+        return -1;
+    }
+    if (a.valor > b.valor) {
+        return 1;
+    }
+    return 0;
+});
+console.log(opcionesDeInversion);
